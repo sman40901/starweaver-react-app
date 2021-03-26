386caf21347946b94b77b0cf4b58d185b1932fb3
sman40901 <sujanmanpr@gmail.com>
2021-03-26 06:40:17 -0700

    used const {} inside of components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+  const isMorning = (new Date()).getHours() < 12;
+  const greetingElement = isMorning ? <h3>Good morning</h3> : <h3>Good evening</h3>;
+
+  const adjective = 'cool';
+  const url = "https://reactjs.org";
+
   return (
     <div className="App">
       <header className="App-header">
+        {greetingElement}
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is so {adjective} !!! <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href={url}
           target="_blank"
           rel="noopener noreferrer"
         >
